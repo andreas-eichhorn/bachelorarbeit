2befe93102dda1be1e01e1510a3bcd3d45eeecf2
id-forgames <andreas.eichhorn1997@gmail.com>
2025-03-02 12:10:48 +0100

    hauptteil geändert + .js hinzugefügt

---
+++ schleifen.js
@@ -0,0 +1,11 @@
+/*Beispiel 1*/
+for (let i = 0; i <= 2; i++) {
+    console.log("foo");
+}
+
+/*Beispiel 2*/
+for (let i = 0; i <= 0; i++) {
+    console.log("foo");
+    console.log("foo");
+    console.log("foo");
+}
