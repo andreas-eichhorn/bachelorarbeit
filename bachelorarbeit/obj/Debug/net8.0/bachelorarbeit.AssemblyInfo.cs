2faa8b8566d33d9254ee1804083d8ef29a57b8ea
Andreas <andreas.eichhorn1997@gmail.com>
2025-04-22 23:30:11 +0200

    hauptteil geändert

--- bachelorarbeit/obj/Debug/net8.0/bachelorarbeit.AssemblyInfo.cs
+++ bachelorarbeit/obj/Debug/net8.0/bachelorarbeit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bachelorarbeit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6abece617f17b647e047279cc6d56e88836c7686")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80826bc8b1969872a21c1fa72574499cb918d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("bachelorarbeit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bachelorarbeit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
